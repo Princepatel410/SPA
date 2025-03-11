a2f7d6a0661e8a1833714233508b1df5dd31adfb
Prince Patel <pricepatel9625@gmail.com>
2025-03-12 00:00:23 +0530

    initialcommit

--- updated practicals/async basic/ callback.js
+++ updated practicals/async basic/ callback.js
@@ -9,3 +9,24 @@ function square(number, callback) {
 square(5, result => {
     console.log("Squared value:", result);
 });
+
+//callback
+function ride(callback) {
+    const arrived = false;
+    if (arrived) {
+        callback(null, "Ride has arrived");
+    }
+    else {
+        callback("Ride was rejected", null);
+    }
+}
+ride((error, message) => {
+    if (error) {
+        console.log("Error: " + error);
+    }
+    else {
+        console.log("Success: " + message);
+    }
+    console.log("Ride has ended");
+}
+);
